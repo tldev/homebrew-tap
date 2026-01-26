8e84750d6c2e4b1cd347cb0e082de1260e5beacf
Tom Johnell <tjohnell@gmail.com>
2026-01-25 21:35:16 -0600

    Add Posturr cask v1.0.11

---
+++ Casks/posturr.rb
@@ -0,0 +1,17 @@
+cask "posturr" do
+  version "1.0.11"
+  sha256 "4faa08aaa5df02816ee1118fa5835ab4d4bede5489799cfd8058777d28579c63"
+
+  url "https://github.com/tldev/posturr/releases/download/v#{version}/Posturr-v#{version}.zip"
+  name "Posturr"
+  desc "Posture monitoring app that blurs your screen when you slouch"
+  homepage "https://github.com/tldev/posturr"
+
+  depends_on macos: ">= :ventura"
+
+  app "Posturr.app"
+
+  zap trash: [
+    "~/Library/Preferences/com.thelazydeveloper.posturr.plist",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,34 @@
+# tldev Homebrew Tap
+
+Homebrew tap for apps by [tldev](https://github.com/tldev).
+
+## Installation
+
+```bash
+brew tap tldev/tap
+```
+
+## Available Apps
+
+### Posturr
+
+A posture monitoring app for macOS that blurs your screen when you slouch.
+
+```bash
+brew install --cask posturr
+```
+
+[More info](https://github.com/tldev/posturr)
+
+## Updating
+
+```bash
+brew upgrade --cask <app-name>
+```
+
+## Uninstalling
+
+```bash
+brew uninstall --cask <app-name>
+brew untap tldev/tap
+```
