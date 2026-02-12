9092f3beed368aaac775d6160ba661ebd5e601bd
Tom Johnell <tjohnell@gmail.com>
2026-02-12 15:25:31 -0600

    Update Posturr to v1.8.2

--- Casks/posturr.rb
+++ Casks/posturr.rb
@@ -1,6 +1,6 @@
 cask "posturr" do
-  version "1.8.1"
-  sha256 "e5127340236908abebfeff83ebb67371be373c88b02c1de8ac7c0979ee6af16c"
+  version "1.8.2"
+  sha256 "a614a2770efc0a3699bd51dd78e1a8e1a9caa88aa15551543bc4f9953d869473"
 
   url "https://github.com/tldev/posturr/releases/download/v#{version}/Posturr-v#{version}.zip"
   name "Posturr"
